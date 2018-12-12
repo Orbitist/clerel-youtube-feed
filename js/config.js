d857c1eb2dca1c9d87ad28baa9d01eed5ceb5bdf
Nicholas Gunner <ngunner@orbitist.com>
2018-12-12 09:03:23 -0500

    touching up styles

--- js/config.js
+++ js/config.js
@@ -7,7 +7,11 @@ $('#tubber-container').tubber({
 	prevPageControl: $('#tubber-prev'),
 	nextPageControl: $('#tubber-next'),
 	mediaViewer: $('#tubber-viewer'),
+  showVideoTitle: true,
+  showVideoDescription: false,
 	templates: {
-		thumbnail: '<img src="{{image_medium}}">'
+		thumbnail: '<img src="{{image_medium}}">',
+    videoTitle: '<p class="title">{{title}}</p>',
+    videoDescription: '<p class="description">{{description}}</p>'
 	}
 });
